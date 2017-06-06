0c164747b9df339789e1060c9cf430ca93fa1543
LunarLincoln <lunarlincoln@lunarlicolnsmbp.attlocal.net>
2017-06-06 10:35:03 -0500

    Initial Commit

---
+++ Pedometer Party/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pedometer Party
+//
+//  Created by LunarLincoln on 6/6/17.
+//  Copyright © 2017 LunarLincoln. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
